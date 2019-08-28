8f790523ab51d928da628036c48de1dbef9328c3
Can Agriboz <agriboz@gmail.com>
2019-08-28 23:05:12 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,8 +61,12 @@ export default {
    */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: 'http://aws.notime.org:3000/'
+    baseURL: '//aws.notime.org:3000/'
     // baseURL: 'https://entplcwsx002.amwater.net:2778'
+  },
+  
+  generate: {
+    fallback: true
   },
 
   /*
